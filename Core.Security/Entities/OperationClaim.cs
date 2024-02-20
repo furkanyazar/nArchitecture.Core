b7df50a731511096d3b9d06f2f93ded8760c0a5d
Furkan Yazar <furkanyazar@proton.me>
2024-02-20 17:01:41 +0300

    feat(Core.Security): update parents of operationClaim and userGroup

--- Core.Security/Entities/OperationClaim.cs
+++ Core.Security/Entities/OperationClaim.cs
@@ -4,22 +4,21 @@ namespace NArchitecture.Core.Security.Entities;
 
 public class OperationClaim<TId> : Entity<TId>
 {
-    public TId ParentId { get; set; }
+    public TId? ParentId { get; set; }
     public string Name { get; set; }
 
     public OperationClaim()
     {
-        ParentId = default!;
         Name = string.Empty;
     }
 
-    public OperationClaim(TId parentId, string name)
+    public OperationClaim(TId? parentId, string name)
     {
         ParentId = parentId;
         Name = name;
     }
 
-    public OperationClaim(TId id, TId parentId, string name)
+    public OperationClaim(TId id, TId? parentId, string name)
         : base(id)
     {
         ParentId = parentId;
--- Core.Security/Entities/UserGroup.cs
+++ Core.Security/Entities/UserGroup.cs
@@ -4,22 +4,21 @@ namespace NArchitecture.Core.Security.Entities;
 
 public class UserGroup<TId> : Entity<TId>
 {
-    public TId ParentId { get; set; }
+    public TId? ParentId { get; set; }
     public string Name { get; set; }
 
     public UserGroup()
     {
-        ParentId = default!;
         Name = string.Empty;
     }
 
-    public UserGroup(TId parentId, string name)
+    public UserGroup(TId? parentId, string name)
     {
         ParentId = parentId;
         Name = name;
     }
 
-    public UserGroup(TId id, TId parentId, string name)
+    public UserGroup(TId id, TId? parentId, string name)
         : base(id)
     {
         ParentId = parentId;
