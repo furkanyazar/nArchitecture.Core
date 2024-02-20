7da4274be4450ce1b071f1b24e319c914c663740
Furkan Yazar <furkanyazar@proton.me>
2024-02-20 16:55:59 +0300

    feat(Core.Security): add parents to operationClaim and userGroup

--- Core.Security/Entities/OperationClaim.cs
+++ Core.Security/Entities/OperationClaim.cs
@@ -4,21 +4,25 @@ namespace NArchitecture.Core.Security.Entities;
 
 public class OperationClaim<TId> : Entity<TId>
 {
+    public TId ParentId { get; set; }
     public string Name { get; set; }
 
     public OperationClaim()
     {
+        ParentId = default!;
         Name = string.Empty;
     }
 
-    public OperationClaim(string name)
+    public OperationClaim(TId parentId, string name)
     {
+        ParentId = parentId;
         Name = name;
     }
 
-    public OperationClaim(TId id, string name)
+    public OperationClaim(TId id, TId parentId, string name)
         : base(id)
     {
+        ParentId = parentId;
         Name = name;
     }
 }
--- Core.Security/Entities/UserGroup.cs
+++ Core.Security/Entities/UserGroup.cs
@@ -4,21 +4,25 @@ namespace NArchitecture.Core.Security.Entities;
 
 public class UserGroup<TId> : Entity<TId>
 {
+    public TId ParentId { get; set; }
     public string Name { get; set; }
 
     public UserGroup()
     {
+        ParentId = default!;
         Name = string.Empty;
     }
 
-    public UserGroup(string name)
+    public UserGroup(TId parentId, string name)
     {
+        ParentId = parentId;
         Name = name;
     }
 
-    public UserGroup(TId id, string name)
+    public UserGroup(TId id, TId parentId, string name)
         : base(id)
     {
+        ParentId = parentId;
         Name = name;
     }
 }
