9e3c001300d1213acf64b0160b6cf884daca3527
Furkan Yazar <furkanyazar@proton.me>
2024-02-20 16:27:57 +0300

    feat(Core.Security): add user group

---
+++ Core.Security/Entities/UserGroup.cs
@@ -0,0 +1,24 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+
+namespace NArchitecture.Core.Security.Entities;
+
+public class UserGroup<TId> : Entity<TId>
+{
+    public string Name { get; set; }
+
+    public UserGroup()
+    {
+        Name = string.Empty;
+    }
+
+    public UserGroup(string name)
+    {
+        Name = name;
+    }
+
+    public UserGroup(TId id, string name)
+        : base(id)
+    {
+        Name = name;
+    }
+}
---
+++ Core.Security/Entities/UserGroupOperationClaim.cs
@@ -0,0 +1,28 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+
+namespace NArchitecture.Core.Security.Entities;
+
+public class UserGroupOperationClaim<TUserGroupId, TOperationClaimId> : Entity<TUserGroupId>
+{
+    public TUserGroupId UserGroupId { get; set; }
+    public TOperationClaimId OperationClaimId { get; set; }
+
+    public UserGroupOperationClaim()
+    {
+        UserGroupId = default!;
+        OperationClaimId = default!;
+    }
+
+    public UserGroupOperationClaim(TUserGroupId userGroupId, TOperationClaimId operationClaimId)
+    {
+        UserGroupId = userGroupId;
+        OperationClaimId = operationClaimId;
+    }
+
+    public UserGroupOperationClaim(TUserGroupId id, TUserGroupId userGroupId, TOperationClaimId operationClaimId)
+        : base(id)
+    {
+        UserGroupId = userGroupId;
+        OperationClaimId = operationClaimId;
+    }
+}
